894b00f0a90c4d1245192e4b5aaadb91dfb74808
Dima Boro <iboro770@gmail.com>
2024-01-31 22:55:11 +0300

    v1.0.0.alpha.24

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
    */
   protected function schedule(Schedule $schedule): void
   {
-    $schedule->command('app:clean-tasks')->everyTeHours();
+    $schedule->command('app:clean-tasks')->everyTwoHours();
     $schedule->command('app:delete-tasks')->everyTenMinutes();
   }
 
