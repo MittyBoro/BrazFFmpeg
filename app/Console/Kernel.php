cbbd2fc7f1ef42058ec633fcd5f6453c820f731d
Dima Boro <iboro770@gmail.com>
2024-01-31 21:40:01 +0300

    v1.0.0.alpha.20

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
   protected function schedule(Schedule $schedule): void
   {
     $schedule->command('app:clean-tasks')->everyTwoHours();
+    $schedule->command('app:refresh-tasks')->everyTenMinutes();
   }
 
   /**
