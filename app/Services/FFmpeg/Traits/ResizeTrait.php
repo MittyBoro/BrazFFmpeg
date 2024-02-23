c537248a77d89e8804240fbeae4b2c1472fa3e4f
Dima Boro <iboro770@gmail.com>
2024-02-23 14:48:31 +0300

    v1.0.0.alpha.51

--- app/Services/FFmpeg/Traits/ResizeTrait.php
+++ app/Services/FFmpeg/Traits/ResizeTrait.php
@@ -3,53 +3,59 @@
 namespace App\Services\FFmpeg\Traits;
 
 use FFMpeg\Format\Video\X264;
+use ProtoneMedia\LaravelFFMpeg\Support\StreamParser;
 
 trait ResizeTrait
 {
-  // 'trailer',
+  // 'resize',
   public function makeResize($quality)
   {
-    $lowBitrate = (new X264())->setKiloBitrate(256);
-    $midBitrate = (new X264())->setKiloBitrate(512);
-    $highBitrate = (new X264())->setKiloBitrate(1024);
-    $megaBitrate = (new X264())->setKiloBitrate(2048);
-    $superBitrate = (new X264())->setKiloBitrate(4096);
-
-    if ($quality <= 240) {
-      $bitrate = $lowBitrate;
-    } elseif ($quality <= 360) {
-      $bitrate = $midBitrate;
-    } elseif ($quality <= 480) {
-      $bitrate = $highBitrate;
-    } elseif ($quality <= 720) {
-      $bitrate = $megaBitrate;
-    } else {
-      $bitrate = $superBitrate;
-    }
-
     $height = intval($quality);
     $width = $this->widthByHeight($quality);
+
+    if ($quality <= 240) {
+      $kiloBitrate = 256;
+    } elseif ($quality <= 360) {
+      $kiloBitrate = 512;
+    } elseif ($quality <= 480) {
+      $kiloBitrate = 1024;
+    } elseif ($quality <= 720) {
+      $kiloBitrate = 2048;
+    } else {
+      $kiloBitrate = 4096;
+    }
+
+    $inputBitrate = $this->ffmpeg->getFormat()->get('bit_rate');
+    $inputKiloBitrate = intval($inputBitrate / 1000);
+    $kiloBitrate = min($inputKiloBitrate, $kiloBitrate);
+
+    $format = (new X264())->setPasses(1)->setKiloBitrate($kiloBitrate);
+
+    $inputFps = ceil(
+      StreamParser::new($this->ffmpeg->getVideoStream())->getFrameRate() ?? 30,
+    );
+    $fps = min($inputFps, 30);
 
     $video = $this->ffmpeg
       ->export()
       ->addFilter(
         '-r',
-        '30',
+        $fps,
         '-preset',
-        'ultrafast', // Выбор предустановки ultrafast для кодирования (очень быстрое кодирование)
+        'veryfast', // Выбор предустановки
         '-c:a',
         'aac', // Выбор кодека AAC для аудио
         '-b:a',
         '128k', // Установка битрейта для аудио на 128 кбит/с
         '-ac',
-        '1', // Установка количества аудиоканалов на 1 (моно)
+        '2', // Установка количества аудиоканалов на 2 (стерео)
         '-pix_fmt',
         'yuv420p', // Установка цветового формата видео (yuv420p)
         '-movflags',
         '+faststart', // Установка флага faststart для ускоренного воспроизведения веб-видео
         '-y', // Принудительное подтверждение перезаписи выходного файла
       )
-      ->inFormat($bitrate)
+      ->inFormat($format)
       ->resize($width, $height)
       ->onProgress(function ($percentage, $remaining) {
         $this->task->progress($percentage);
